dd191ab5a6c702903c080400d7c1d2e8fe0db02a
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2010-05-31 14:10:56 -0700

    restful_authentication added

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100530025808) do
+ActiveRecord::Schema.define(:version => 20100531210920) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -19,5 +19,19 @@ ActiveRecord::Schema.define(:version => 20100530025808) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+  end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
 end
